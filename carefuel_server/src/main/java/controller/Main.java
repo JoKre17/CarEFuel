6d6bdc6c238b9eaef9d2b9a0cb64e404b3251769
Wolfgang Gritz <wolfgang.gritz@hotmail.de>
2017-11-02 13:57:16 +0100

    added CSV Parser to project, added connection to the database, added entity class for GasStation

--- carefuel_server/src/main/java/controller/Main.java
+++ carefuel_server/src/main/java/controller/Main.java
@@ -1,7 +1,12 @@
 package controller;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import model.GasStation;
+import util.InputParser;
 
 
 /**
@@ -16,5 +21,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Main {
 	public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+
+        InputParser inputParser = new InputParser();
+		DatabaseHandler databaseHandler = new DatabaseHandler();
+		
+		List<GasStation> gasStationslist = inputParser.getAllGasStationsFromCSV(System.getProperty("user.dir") + "/../input_files/gasstations.csv");
+//		databaseHandler.setup();
+//		databaseHandler.createGasStations(gasStationslist);
+//		databaseHandler.exit();
     }
 }
