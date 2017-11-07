d425558f3ed25346be1c35fc70d8a5121232d1de
Josef Kriegel <josef.kriegel@web.de>
2017-11-07 15:53:06 +0100

    Renamed projects, unified package names to carefuel.*, added logging dependencies and build configuration.

--- Grundanforderung/src/main/java/com/carefuel/control/PricePredictor.java
+++
@@ -1,5 +0,0 @@
-package com.carefuel.control;
-
-public class PricePredictor {
-
-}
---
+++ carefuel_extended/src/main/java/carefuel/controller/Main.java
@@ -0,0 +1,41 @@
+package carefuel.controller;
+
+import java.util.Date;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import carefuel.model.GasStation;
+import carefuel.util.InputParser;
+
+
+/**
+ * 
+ * Main is the entry class of the application. It starts the webserver (with automatically embedded
+ * tomcat servlet container, which means this code can be run as a standalone application) and searches
+ * for classes with the Controller annotation in this package and should therefore use the
+ * RequestController class for handling all requests.
+ *
+ */
+@SpringBootApplication
+public class Main {
+	
+	private static final Logger log = LogManager.getLogger(Main.class);
+	
+	public static void main(String[] args) {
+		log.info("Startup of CarEFuel_Extended at " + new Date().toString());
+
+		SpringApplication.run(Main.class, args);
+
+        InputParser inputParser = new InputParser();
+		DatabaseHandler databaseHandler = new DatabaseHandler();
+		
+		List<GasStation> gasStationslist = inputParser.getAllGasStationsFromCSV(System.getProperty("user.dir") + "/../input_files/gasstations.csv");
+//		databaseHandler.setup();
+//		databaseHandler.createGasStations(gasStationslist);
+//		databaseHandler.exit();
+    }
+}
