3f68f3230bfd34d86c061d0bc74f6de7ab74c057
Josef Kriegel <josef.kriegel@web.de>
2017-11-07 15:53:06 +0100

    Renamed projects, unified package names to carefuel.*, added logging dependencies and build configuration.

---
+++ carefuel_basic/src/main/java/carefuel/app/App.java
@@ -0,0 +1,32 @@
+package carefuel.app;
+
+import java.util.Date;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+	private static final Logger log = LogManager.getLogger(App.class);
+
+	public static void main(String[] args) {
+		log.info("Startup of CarEFuel_Basic at " + new Date().toString());
+
+		testLog();
+		
+	}
+
+	private static void testLog() {
+		// notice that log.debug also gets printed even though the root logger is set to level info
+		// this is due to the application logger configuration see @log4j2.xml
+		log.info("Hello World!");
+		log.debug("Hello World!");
+		log.error("Hello World!");
+		log.fatal("Hello World!");
+	}
+
+}
---
+++ carefuel_extended/src/main/java/carefuel/controller/DatabaseHandler.java
@@ -0,0 +1,78 @@
+package carefuel.controller;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import carefuel.model.GasStation;
+
+/**
+ * 
+ * This class manages the database containing all information about gas stations and prices and
+ * offers functionality to request and update data.
+ *
+ */
+public class DatabaseHandler {
+
+	protected SessionFactory sessionFactory;
+
+	/**
+	 * loads configuration from hibernate.cfg.xml and sets up the session
+	 */
+	public void setup() {
+		// code to load Hibernate Session factory
+		final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+		        .configure() // configures settings from hibernate.cfg.xml
+		        .build();
+		try {
+		    sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+		} catch (Exception ex) {
+		    StandardServiceRegistryBuilder.destroy(registry);
+		}
+	}
+
+	/**
+	 * closes the session
+	 */
+	public void exit() {
+		// code to close Hibernate Session factory
+		sessionFactory.close();
+	}
+
+	/**
+	 * GasStations will be created in the DB, if they are not already created
+	 * @param list of GasStations
+	 * 
+	 */
+	public void createGasStations(List<GasStation> list) {
+		// code to save a gas station
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		
+		for (GasStation gas : list) {
+			session.saveOrUpdate(gas);
+		}
+		
+		session.getTransaction().commit();
+		session.close();
+	}
+
+	/**
+	 * returns all GasStations in the database in a list
+	 * @return all GasStations
+	 */
+	@SuppressWarnings({ "deprecation", "unchecked" })
+	public List<GasStation> getAllGasStations() {
+		List<GasStation> gasStations = new LinkedList();
+		
+		Session session = sessionFactory.openSession();
+		gasStations = session.createCriteria(GasStation.class).list();
+		System.out.println(gasStations.get(0));
+		return gasStations;
+	}
+}
