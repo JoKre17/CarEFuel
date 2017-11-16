c87d348d55e00547678337cdbfe757ac35b87b22
Josef Kriegel <josef.kriegel@web.de>
2017-11-16 12:49:45 +0100

    Moved static files into static and added Rest call functionality in control.js

--- carefuel_extended/src/main/java/carefuel/controller/RequestController.java
+++ carefuel_extended/src/main/java/carefuel/controller/RequestController.java
@@ -1,8 +1,11 @@
 package carefuel.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +21,8 @@ import carefuel.model.GasStation;
 @RestController
 @RequestMapping("rest/")
 public class RequestController {
-	// Insert request handler functions here
+
+	private final static Logger log = LogManager.getLogger(RequestMapping.class);
 
 	@RequestMapping(value = "station/{id}", method = RequestMethod.GET, produces = "application/json")
 	@ResponseBody
@@ -26,4 +30,19 @@ public class RequestController {
 		// return "{station: {id: " + id + "}}";
 		return new GasStation();
 	}
+
+	@RequestMapping(value = "path", method = RequestMethod.GET, produces = "application/json")
+	@ResponseBody
+	public String getGasStationById(@RequestParam(value = "from", required = true) String from,
+			@RequestParam(value = "to", required = true) String to,
+			@RequestParam(value = "capacity", required = false) Integer capacity,
+			@RequestParam(value = "consumption", required = false) Double consumption) {
+		// return "{station: {id: " + id + "}}";
+		log.info("received path request");
+		String response = "{\"from\":\"" + from + "\", \"to\":\"" + to + "\", \"capacity\":" + capacity
+				+ ", \"consumption\":" + consumption + "}";
+		log.info(response);
+		return response;
+	}
+
 }
