898df1b83e5b26b5a91d65d00671683e4b1dce09
Wolfgang Gritz <wolfgang.gritz@hotmail.de>
2017-12-07 16:46:54 +0100

    route is displayed with markers and path
    Former-commit-id: e902e359a5ae1e7a2125724401a7ae90723c35da

--- carefuel_extended/src/main/resources/static/js/control.js
+++ carefuel_extended/src/main/resources/static/js/control.js
@@ -6,96 +6,109 @@ function submitButtonPressed() {
 	var to = document.getElementById('to').value;
 	var capacity = document.getElementById('capacity').value;
 	var consumption = document.getElementById('consumption').value;
-	
-	if(capacity == "") {
+
+	if (capacity == "") {
 		capacity = 3;
 	}
-	
-	if(consumption == "") {
+
+	if (consumption == "") {
 		consumption = 5.6;
 	}
-	
-	var request = {'from': from, 'to': to, 'capacity': capacity, 'consumption': consumption};
+
+	var request = {
+		'from' : from,
+		'to' : to,
+		'capacity' : capacity,
+		'consumption' : consumption
+	};
 	var GET_Request_url = "rest/path?"
-	for(var key in request) {
+	for ( var key in request) {
 		GET_Request_url += key + "=" + request[key] + "&"
 	}
-	GET_Request_url = GET_Request_url.substring(0, GET_Request_url.length-1)
+	GET_Request_url = GET_Request_url.substring(0, GET_Request_url.length - 1)
 	request['request'] = GET_Request_url;
-	
+
 	console.log(request['request']);
-	
-	$.ajax({ 
-        type: "GET",
-        dataType: "json",
-        url: request['request'],
-        success: function(data){ 
-        	console.log("receive data:");
-           console.log(data);
-        },
-        complete: function(xhr, textStatus) {
-            console.log(xhr.status);
-        } 
-    });
+
+	$.ajax({
+		type : "GET",
+		dataType : "json",
+		url : request['request'],
+		success : function(data) {
+			console.log("receive data:");
+			console.log(data);
+		},
+		complete : function(xhr, textStatus) {
+			console.log(xhr.status);
+		}
+	});
 
 }
 
 function hideButtonPressed() {
 	var container = document.getElementById("controlContainer");
 	var style = window.getComputedStyle(container);
-	
+
 	var width = style.getPropertyValue('width');
-	
+
 	console.log();
-	if(container.style.left == 0 || container.style.left == "0px" || container.style.left == "0%") {
-		$(container).animate({left: "-"+ width}, {
-			complete: function () {
+	if (container.style.left == 0 || container.style.left == "0px"
+			|| container.style.left == "0%") {
+		$(container).animate({
+			left : "-" + width
+		}, {
+			complete : function() {
 				container.style.maxWidth = 'none';
 				container.style.left = -CONTROL_MAX_REL_WIDTH * 100 + "%";
 			}
 		});
-		
+
 		var $elem = $('#hide');
-	    // we use a pseudo object for the animation
-	    // (starts from `0` to `angle`), you can name it as you want
-	    $({deg: 0}).animate({deg: 180}, {
-	        duration: 350,
-	        step: function(now) {
-	            $elem.css({
-	                transform: 'rotate(' + now + 'deg)'
-	            });
-	        }
-	    });
-		
+		// we use a pseudo object for the animation
+		// (starts from `0` to `angle`), you can name it as you want
+		$({
+			deg : 0
+		}).animate({
+			deg : 180
+		}, {
+			duration : 350,
+			step : function(now) {
+				$elem.css({
+					transform : 'rotate(' + now + 'deg)'
+				});
+			}
+		});
+
 	} else {
 		container.style.maxWidth = CONTROL_MAX_WIDTH + 'px';
 		container.style.left = "-" + width;
-		
-		$(container).animate({left: "0%"});
-		
-		
+
+		$(container).animate({
+			left : "0%"
+		});
+
 		var $elem = $('#hide');
-	    // we use a pseudo object for the animation
-	    // (starts from `0` to `angle`), you can name it as you want
-	    $({deg: 180}).animate({deg: 360}, {
-	        duration: 350,
-	        step: function(now) {
-	            $elem.css({
-	                transform: 'rotate(' + now + 'deg)'
-	            });
-	        }
-	    }, {
-			complete: function () {
+		// we use a pseudo object for the animation
+		// (starts from `0` to `angle`), you can name it as you want
+		$({
+			deg : 180
+		}).animate({
+			deg : 360
+		}, {
+			duration : 350,
+			step : function(now) {
 				$elem.css({
-	                transform: 'rotate(0deg)'
-	            });
+					transform : 'rotate(' + now + 'deg)'
+				});
+			}
+		}, {
+			complete : function() {
+				$elem.css({
+					transform : 'rotate(0deg)'
+				});
 			}
 		});
 	}
-	
-	
-	
-
 
 }
 
@@ -103,14 +116,14 @@ var window_width = $(window).width();
 
 window.onresize = function(event) {
 	var new_window_width = $(window).width();
-	
-	if(window_width < 800 && new_window_width > 800) {
+
+	if (window_width < 800 && new_window_width > 800) {
 		var container = document.getElementById("controlContainer");
 		var style = window.getComputedStyle(container);
-		
+
 		container.style.maxWidth = CONTROL_MAX_WIDTH + 'px';
 		container.style.left = "0%";
 	}
-	
+
 	window_width = new_window_width;
 };
--- carefuel_extended/src/main/resources/static/js/map.js
+++ carefuel_extended/src/main/resources/static/js/map.js
@@ -1,14 +1,130 @@
-
 var apiKey = "AIzaSyAvS9-jUE8zO6234VVC5_O2GvkDLtN27K0";
 var map;
+var directionsService;
+var directionsDisplay;
 
+/**
+ * initialization method for the map
+ * loads the api
+ * @returns
+ */
 function initMap() {
+	directionsService = new google.maps.DirectionsService;
+	directionsDisplay = new google.maps.DirectionsRenderer
+	
 	map = new google.maps.Map(document.getElementById('map'), {
-		center : {
-			lat : 52.37052,
-			lng : 9.73322
-		},
 		zoom : 8,
-		disableDefaultUI: true
+		disableDefaultUI : true
 	});
+	directionsDisplay.setMap(map);
+	
+	positions = [{
+				lng: 9.5167,
+				lat: 52.5167
+			}, {
+				lng: 9.701,
+				lat: 52.326
+			}, {
+				lng: 9.791,
+				lat: 52.426
+			}, {
+				lng: 9.8367,
+				lat: 52.2167
+			}
+	]
+
+	centerToUserPosition();
+	calculateAndDisplayRoute(positions);
 }
+
+/**
+ * centers the map to the users position
+ * @returns
+ */
+function centerToUserPosition() {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(position) {
+			var pos = {
+				lat : position.coords.latitude,
+				lng : position.coords.longitude
+			};
+
+			map.setCenter(pos);
+			return pos;
+		}, function() {
+			handleLocationError(true, infoWindow, map.getCenter());
+		});
+	} else {
+		// Browser doesn't support Geolocation
+		handleLocationError(false, infoWindow, map.getCenter());
+		return null;
+	}
+}
+
+/**
+ * displays markers on given positions
+ * @param positions
+ * 		positions as array of objects with lng and lat
+ * @returns
+ */
+function displayMarkers(positions){
+	for (pos in positions){
+		
+		position = positions[pos];
+		console.log(position);
+		
+		var marker = new google.maps.Marker({
+	        position: position,
+	        map: map
+	    });
+	}
+}
+
+/**
+ * draws a polyline between coordinates. 0-1, 1-2, 2-3...
+ * @param positions
+ * 		route in the right order
+ * @returns
+ */
+function calculateAndDisplayRoute(positions) {
+
+	var waypoints = [];
+	
+	for(var i=1; i<positions.length-1; i++){
+		waypoints.push({
+			location : positions[i],
+			stopover : true
+		})
+	}
+	
+    directionsService.route({
+      origin: positions[0],
+      destination: positions[positions.length-1],
+      waypoints: waypoints,
+      optimizeWaypoints: true,
+      travelMode: 'DRIVING'
+    }, function(response, status) {
+    	console.log(response);
+      if (status === 'OK') {
+        directionsDisplay.setDirections(response);
+        var route = response.routes[0];
+        var summaryPanel = document.getElementById('routeContainer');
+        summaryPanel.innerHTML = '';
+        
+        // For each route, display summary information.
+        summaryPanel.innerHTML += '<h4>Start</h4>';
+        summaryPanel.innerHTML += route.legs[0].start_address;
+        summaryPanel.innerHTML += '<br>';
+        
+        for (var i = 1; i < route.legs.length; i++) {
+          summaryPanel.innerHTML += '<h5>Stop: ' + (i) + '</h5>';
+          summaryPanel.innerHTML += route.legs[i].start_address + '<br>';
+          summaryPanel.innerHTML += route.legs[i].distance.text + '<br>';
+        }
+        
+        summaryPanel.innerHTML += '<h4>Ziel</h4>';
+        summaryPanel.innerHTML += route.legs[route.legs.length-1].end_address;
+        
+      }
+    });
+  }
